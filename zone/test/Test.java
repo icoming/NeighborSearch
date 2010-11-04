4f2cf81b416a7e1f610ff0311a88f2cc7e201701
Da Zheng <zhengda1936@gmail.com>
2010-11-04 13:59:04 -0400

    fix a bug in brutal force version.

--- zone/test/Test.java
+++ zone/test/Test.java
@@ -1,18 +1,23 @@
 package zone.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import zone.BlockIDWritable;
 import zone.Star;
 
 public class Test {
-	private static int splitSize = 1000;	// the number of records
 	private static double theta = 0.1;
 	public static void main (String[] args) {
-		Star stars[] = new Star[splitSize];
+		int splitSize;	// the number of records
+		Star stars[];
 		try {
+			File file = new File (args[0]);
+			splitSize = (int) (file.length() / Star.storeSize);
 			FileInputStream in = new FileInputStream(args[0]);
+			stars = new Star[splitSize];
 			byte[] bytes = new byte[Star.storeSize];
 			for (int i = 0; i < splitSize; i++) {
 				int size = in.read(bytes);
@@ -29,18 +34,14 @@ public class Test {
 			e.printStackTrace();
 			return;
 		}
-		
+
 		int num = 0;
 		for (int i = 0; i < splitSize; i++) {
-			for (int j = 0; j < splitSize; j++) {
-				if (i == j)
-					continue;
-				if (stars[i].objID > stars[j].objID)
-					continue;
-				
+			for (int j = i + 1; j < splitSize; j++) {
 				if (stars[i].x * stars[j].x + stars[i].y * stars[j].y + stars[i].z * stars[j].z > Math.cos(Math.toRadians(theta))) {
-					System.out.println("(66,9)\t(" + stars[i] + "," + stars[j] + ")");
-					System.out.println("(66,9)\t(" + stars[j] + "," + stars[i] + ")");
+					BlockIDWritable loc = new BlockIDWritable(stars[i].ra, stars[i].dec);
+					System.out.println(loc + "\t(" + stars[i] + "," + stars[j] + ")");
+					System.out.println(loc + "\t(" + stars[j] + "," + stars[i] + ")");
 					num += 2;
 				}
 			}
