35f3c48ab00ea7f8e3d79891dcd906263d5be7d5
Da Zheng <zhengda1936@gmail.com>
2010-11-05 19:17:28 -0400

    add a simplified getSplits.

--- zone/io/StarInputFormat.java
+++ zone/io/StarInputFormat.java
@@ -1,7 +1,12 @@
 package zone.io;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapred.InputSplit;
@@ -10,6 +15,7 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.net.NetworkTopology;
 
 import zone.Star;
 
@@ -21,5 +27,24 @@ public class StarInputFormat extends FileInputFormat<LongWritable, Star> {
 	    reporter.setStatus(split.toString());
 		return new StarReader(job, (FileSplit) split);
 	}
+	
+	// TODO I need to rewrite this.
+	 /** Splits files returned by {@link #listStatus(JobConf)} when
+	   * they're too big.*/
+	 public InputSplit[] getSplits(JobConf job, int numSplits)
+	    throws IOException {
+	    FileStatus[] files = listStatus(job);
+	    ArrayList<FileSplit> splits = new ArrayList<FileSplit>(numSplits);
+	
+	    long totalSize = 0;                           // compute total size
+	    for (FileStatus file: files) {                // check we have valid files
+	      if (file.isDir()) {
+	        throw new IOException("Not a file: "+ file.getPath());
+	      }
+	      splits.add(new FileSplit(file.getPath(), 0, file.getLen(), job));
+	    }
+
+	    return splits.toArray(new FileSplit[splits.size()]);
+	  }
 
 }
