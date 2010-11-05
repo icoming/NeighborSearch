cb1b5d1a3f227f0fa3bde29d087b85d759425705
Da Zheng <zhengda1936@gmail.com>
2010-11-05 12:29:25 -0400

    fix a bug in array2Long

--- zone/util/Converter.java
+++ zone/util/Converter.java
@@ -37,7 +37,7 @@ public class Converter {
 	public static long array2Long(byte[] bytes, int offset) {
 		return (((long) bytes[offset + 7] & 0xFF) << 56) + (((long) bytes[offset + 6] & 0xFF) << 48)
 		+ (((long) bytes[offset + 5] & 0xFF) << 40) + (((long) bytes[offset + 4] & 0xFF) << 32)
-		+ (bytes[offset + 3] & 0xFF << 24) + ((bytes[offset + 2] & 0xFF) << 16)
+		+ ((bytes[offset + 3] & 0xFF) << 24) + ((bytes[offset + 2] & 0xFF) << 16)
 		+ ((bytes[offset + 1] & 0xFF) << 8) + (bytes[offset + 0] & 0xFF);
 	}
 	
