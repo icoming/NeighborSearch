e92eda3125e24f3937bf73b4de70520f47de4d8d
Da Zheng <zhengda1936@gmail.com>
2010-12-16 19:11:55 -0500

    provide the correct locations of splits

--- zone/newapi/io/StarInputFormat.java
+++ zone/newapi/io/StarInputFormat.java
@@ -36,6 +36,39 @@ public class StarInputFormat extends FileInputFormat<LongWritable, Star> {
 	    return codec == null;
 	  }
 
+
+	  protected int getBlockIndex(BlockLocation[] blkLocations, long offset,
+			  long length) {
+		  long end = offset + length;
+		  for (int i = 0; i < blkLocations.length; i++) {
+			  // is the offset inside this block?
+			  if ((blkLocations[i].getOffset() <= offset)
+					  && (offset < blkLocations[i].getOffset()
+						  + blkLocations[i].getLength())) {
+				  if (i == blkLocations.length - 1)
+					  return i;
+				  // I assume the location of the next block is at i+1
+				  // if the end of the split is in the next block
+				  else if ((blkLocations[i + 1].getOffset() < end)
+						  && (end < blkLocations[i + 1].getOffset()
+							  + blkLocations[i + 1].getLength())
+						  // if more data is in the next block
+						  && end - blkLocations[i + 1].getOffset() > blkLocations[i]
+						  .getOffset()
+						  + blkLocations[i].getLength() - offset) {
+					  return i + 1;
+						  }
+				  else
+					  return i;
+						  }
+		  }
+		  BlockLocation last = blkLocations[blkLocations.length - 1];
+		  long fileLength = last.getOffset() + last.getLength() - 1;
+		  throw new IllegalArgumentException("Offset " + offset
+				  + " is outside of file (0.." + fileLength + ")");
+	  }
+
+
 	@Override
 	public List<InputSplit> getSplits(JobContext job) throws IOException {
 		long minSize = Math.max(getFormatMinSplitSize(), getMinSplitSize(job));
@@ -52,17 +85,17 @@ public class StarInputFormat extends FileInputFormat<LongWritable, Star> {
 					length);
 			if ((length != 0) && isSplitable(job, path)) {
 				long blockSize = file.getBlockSize();
-				/* TODO is this really a right way? 
-				 * when I use setMinInputSplitSize and setMaxInputSplitSize,
-				 * I get a different number of mapping output records, 
-				 * but the same number of mapping input records. why? */
+				// TODO is this really a right way? 
+				// when I use setMinInputSplitSize and setMaxInputSplitSize,
+				// I get a different number of mapping output records, 
+				// but the same number of mapping input records. why?
 				long splitSize = computeSplitSize(blockSize, minSize, maxSize);
 				splitSize = (splitSize / starSize) * starSize; 
 
 				long bytesRemaining = length;
 				while (((double) bytesRemaining) / splitSize > SPLIT_SLOP) {
 					int blkIndex = getBlockIndex(blkLocations, length
-							- bytesRemaining);
+							- bytesRemaining, splitSize);
 					splits.add(new FileSplit(path, length - bytesRemaining,
 							splitSize, blkLocations[blkIndex].getHosts()));
 					bytesRemaining -= splitSize;
